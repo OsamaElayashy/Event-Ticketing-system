b3e7829d45059f77ba1d45feaab8f6fe78c9ba90
OsamaElayashy <osama.elayashy@student.giu-uni.de>
2025-03-05 08:26:32 +0200

    z

---
+++ User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose")
+
+const UserSchema = new mongoose.Schema( {
+    name:{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true
+    },
+    profilePicture: {
+        type: String
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ["StandardUser" , "Organizer" , "Admin"],
+        required: true
+    }},  { timestamps: true}
+);
+
+//Mongoose Model
+const User = mongoose.model("User" , UserSchema);
+module.exports = User;
+
