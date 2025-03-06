5b0cabd7df1187c7bf2e2786a9d61d23a9d6ab02
mo-sl410 <mayasasadodo@gmail.com>
2025-03-06 13:49:15 +0200

    creating event schema

---
+++ Event.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose")
+
+const EventSchema = new mongoose.Schema( {
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    Date: {
+        type: Date,
+        required: true
+    },
+    location: {
+        type: String,
+        required: true
+    },
+    category: {
+        type: String,
+        required: true
+    },
+    image: {
+        type: String,
+        required: true
+    },
+    ticketPrice: {
+        type: Number,
+        required: true
+    },
+    totaltickets: {
+        type: Number,
+        required: true
+    },
+    remainingTickets: {
+        type: Number,
+        required: true,
+        default: function () { return this.totalTickets; } 
+    },
+    Organizer: {
+        type: mongoose.Schema.Types.ObjectId, 
+        ref: 'User',
+        required: true
+    }
+
+},  { timestamps: true}
+);
+module.exports = mongoose.model("Event", EventSchema);
